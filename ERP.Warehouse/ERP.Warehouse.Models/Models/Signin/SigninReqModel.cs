c00f15b4599714ab3c53c638bb2f9056736f675c
HanHtooAung <hanhtooaung@acedatasystems.com>
2026-06-22 21:22:08 +0630

    fix : namespace for signin model

---
+++ ERP.Warehouse/ERP.Warehouse.Models/Models/Signin/SigninReqModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ERP.Warehouse.Models.Models.Signin.Signin;
+
+public class SigninReqModel
+{
+    [Required(ErrorMessage = "Please enter your UserName.")]
+    public string UserName { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [StringLength(6, MinimumLength = 6, ErrorMessage = "Passcode must be 6 digits.")]
+    public string Password { get; set; }
+}
+
+public class SignoutReqModel
+{
+    public string UserName { get; set; } = string.Empty;
+
+    public string SessionToken { get; set; } = string.Empty;
+}
---
+++ ERP.Warehouse/ERP.Warehouse.Models/Models/Signin/SigninResModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ERP.Warehouse.Models.Models.Signin.Signin;
+
+public class SigninResModel
+{
+    public string UserId { get; set; }
+    public string SessionId { get; set; }
+}
