fa843a94f5e6763e8b1ed4227249b7606d28132d
HanHtooAung <hanhtooaung@acedatasystems.com>
2026-06-30 11:08:49 +0630

    feat : Edit form first

--- ERP.Warehouse/ERP.Warehouse.App/Components/Pages/WarehouseUser/WarehouseUserList/WarehouseUserList.razor.cs
+++ ERP.Warehouse/ERP.Warehouse.App/Components/Pages/WarehouseUser/WarehouseUserList/WarehouseUserList.razor.cs
@@ -1,5 +1,6 @@
 ﻿using ERP.Warehouse.Models.Models.WarehouseUserList;
 using Microsoft.JSInterop;
+using MudBlazor;
 using WSIMS_ERP.Shared;
 using WSIMS_ERP.Shared.Models;
 using WSIMS_ERP.Shared.Models.PageSetting;
@@ -10,9 +11,9 @@ namespace ERP.Warehouse.App.Components.Pages.WarehouseUser.WarehouseUserList;
 public partial class WarehouseUserList
 {
     private WarehouseUserReqModel _reqModel = new();
-    private WarehouseUserRepModel _repModel = new();
     private IEnumerable<WarehouseUserModel> _model = new List<WarehouseUserModel>();
     private bool hover = true;
+    private bool _readOnly;
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
@@ -49,7 +50,23 @@ public partial class WarehouseUserList
         }
         catch(Exception ex)
         {
-
+            await _injectService.DisableLoading();
+            _logger.LogCustomError(ex);
+            await _injectService.ErrorDialogMessage(ex.Message);
         }
+    }
+
+    private void Edit(WarehouseUserModel item)
+    {
+
+    }
+
+    private void Cancel(WarehouseUserModel item)
+    {
+
+    }
+    private async Task Save(WarehouseUserModel item)
+    {
+
     }
 }
