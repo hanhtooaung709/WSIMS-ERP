cd4e743532d9e69d7eee9f04fdc4f3692158adf5
HanHtooAung <hanhtooaung115@gmai.com>
2026-07-10 10:45:40 +0630

    feat : Get/Create/Edit/Update/Delete/Details Ui from and connect api

---
+++ ERP.Warehouse/ERP.Warehouse.App/Components/Pages/Box/Box.razor.cs
@@ -0,0 +1,254 @@
+﻿using DocumentFormat.OpenXml.Drawing.Diagrams;
+using ERP.Warehouse.Models.Models.Box;
+using ERP.Warehouse.Models.Models.WarehouseUser.WarehouseUserList;
+using MudBlazor;
+using WSIMS_ERP.Shared;
+using WSIMS_ERP.Shared.Enums;
+
+namespace ERP.Warehouse.App.Components.Pages.Box;
+
+public partial class Box
+{
+    private BoxReqModel _reqModel = new();
+    private IEnumerable<BoxModel> _model = new List<BoxModel>();
+    private BoxEditModel _edit = new();
+    private BoxDetailModel _details = new();
+
+    private MudDataGrid<BoxModel> _elementGrid = default!;
+    private EnumFormType _formType = EnumFormType.List;
+    private bool hover = true;
+    private bool _readOnly;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        try
+        {
+            if (firstRender)
+            {
+                await List();
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogCustomError(ex);
+            await _injectService.ErrorDialogMessage(ex.Message);
+        }
+    }
+
+    #region Get/Create/Edit/Update/Delete/Details
+
+    private async Task List()
+    {
+        try
+        {
+            await _injectService.EnableLoading();
+            var result = await _apiService.Get(_reqModel);
+            await _injectService.DisableLoading();
+
+            if (result.IsError)
+            {
+                await _injectService.ShowDialog(result);
+                _reqModel = new();
+                return;
+            }
+
+            _model = result.Data!.list!;
+            StateHasChanged();
+        }
+        catch (Exception ex)
+        {
+            await _injectService.DisableLoading();
+            _logger.LogCustomError(ex);
+            await _injectService.ErrorDialogMessage(ex.Message);
+        }
+    }
+
+    private async Task Create()
+    {
+        var modle = new BoxModel();
+        await _elementGrid.SetEditingItemAsync(modle);
+        _formType = EnumFormType.Create;
+    }
+
+    private async Task Save(BoxModel reqModel)
+    {
+        try
+        {
+            #region Create
+
+            if (_reqModel.BoxId.IsNullOrEmpty())
+            {
+                bool confirm = await _injectService.ShowCreateDialog();
+                if (!confirm) return;
+
+                await _injectService.EnableLoading();
+                var result = await _apiService.Create(_reqModel);
+                await _injectService.DisableLoading();
+
+                if (result.IsError)
+                {
+                    await _injectService.ShowDialog(result);
+                    _reqModel = new();
+                    return;
+                }
+                await _injectService.ShowDialog(result);
+            }
+
+            #endregion
+
+            #region Update
+
+            else
+            {
+                bool confirm = await _injectService.ShowUpdateDialog();
+                if (!confirm) return;
+
+                await _injectService.EnableLoading();
+                var result = await _apiService.Update(_reqModel);
+                await _injectService.DisableLoading();
+
+                if (result.IsError)
+                {
+                    await _injectService.ShowDialog(result);
+                    _reqModel = new();
+                    return;
+                }
+                await _injectService.ShowDialog(result);
+            }
+
+            #endregion
+
+            _reqModel = new();
+            await List();
+        }
+
+        catch (Exception ex)
+        {
+            await _injectService.DisableLoading();
+            _logger.LogCustomError(ex);
+            await _injectService.ErrorDialogMessage(ex.Message);
+        }
+    }
+
+    private async Task Edit(BoxModel reqModel)
+    {
+        try
+        {
+            if (reqModel is null || string.IsNullOrEmpty(reqModel.BoxId))
+            {
+                _formType = EnumFormType.Create;
+                return;
+            }
+
+            _edit.BoxId = reqModel.BoxId!;
+
+            await _injectService.EnableLoading();
+            var result = await _apiService.Edit(_edit);
+            await _injectService.DisableLoading();
+
+            if (result.IsError)
+            {
+                await _injectService.ShowDialog(result);
+                return;
+            }
+
+            _reqModel.BoxId = result.Data.BoxId;
+            _reqModel.BoxCode = result.Data.BoxCode;
+            _reqModel.Type = result.Data.Type;
+            _reqModel.Size = result.Data.Size;
+            _reqModel.TareWeight = result.Data.TareWeight;
+            _reqModel.MaxNetWeight = result.Data.MaxNetWeight;
+
+            _formType = EnumFormType.Edit;
+            StateHasChanged();
+        }
+        catch (Exception ex)
+        {
+            await _injectService.DisableLoading();
+            _logger.LogCustomError(ex);
+            await _injectService.ErrorDialogMessage(ex.Message);
+        }
+    }
+
+    private void Cancel()
+    {
+        try
+        {
+            _reqModel = new();
+            StateHasChanged();
+            List();
+            _formType = EnumFormType.List;
+        }
+        catch (Exception ex)
+        {
+            _injectService.DisableLoading();
+            _logger.LogCustomError(ex);
+            _injectService.ErrorDialogMessage(ex.Message);
+        }
+    }
+
+    private async Task Delete(BoxModel reqModel)
+    {
+        try
+        {
+            bool confirm = await _injectService.Confirm();
+            if (!confirm)
+            {
+                return;
+            }
+
+            _edit.BoxId = reqModel.BoxId!;
+
+            await _injectService.EnableLoading();
+            var result = await _apiService.Delete(_edit);
+            await _injectService.DisableLoading();
+
+            if (result.IsError)
+            {
+                await _injectService.ShowDialog(result);
+                return;
+            }
+            await _injectService.ShowDialog(result);
+
+            await List();
+            _formType = EnumFormType.List;
+            StateHasChanged();
+        }
+        catch (Exception ex)
+        {
+            await _injectService.DisableLoading();
+            _logger.LogCustomError(ex);
+            await _injectService.ErrorDialogMessage(ex.Message);
+        }
+    }
+
+    private async Task Details(BoxModel reqModel)
+    {
+        try
+        {
+            _edit.BoxId = reqModel.BoxId!;
+
+            await _injectService.EnableLoading();
+            var result = await _apiService.Details(_edit);
+            await _injectService.DisableLoading();
+
+            if (result.IsError)
+            {
+                await _injectService.ShowDialog(result);
+                return;
+            }
+
+            _details = result.Data!;
+            _formType = EnumFormType.Detail;
+            StateHasChanged();
+        }
+        catch (Exception ex)
+        {
+            await _injectService.DisableLoading();
+            _logger.LogCustomError(ex);
+            await _injectService.ErrorDialogMessage(ex.Message);
+        }
+    }
+
+    #endregion
+}
