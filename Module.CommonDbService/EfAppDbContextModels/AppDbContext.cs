97390e996c8c5a11748ebba59f214d82e911bdee
HanHtooAung <hanhtooaung115@gmail.com>
2026-07-17 15:49:04 +0630

    fix : db scaffolding

--- Module.CommonDbService/EfAppDbContextModels/AppDbContext.cs
+++ Module.CommonDbService/EfAppDbContextModels/AppDbContext.cs
@@ -53,6 +53,10 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<TblWarehouseUserSession> TblWarehouseUserSessions { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=ep-bold-bonus-azbxwgr9.c-3.ap-southeast-1.aws.neon.tech;Database=WSIMS-ERP;Username=neondb_owner;Password=npg_94yTctfwHjEB;Port=5432;SSL Mode=Require;");
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<TblBox>(entity =>
