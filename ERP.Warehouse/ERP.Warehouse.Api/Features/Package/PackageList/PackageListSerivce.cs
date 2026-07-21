6fbdea6bd01c568b1d8292dc460a5c356ae07201
HanHtooAung <hanhtooaung115@gmail.com>
2026-07-21 16:43:48 +0630

    feat : make Sp_GetPackageList

--- ERP.Warehouse/ERP.Warehouse.Api/Features/Package/PackageList/PackageListSerivce.cs
+++ ERP.Warehouse/ERP.Warehouse.Api/Features/Package/PackageList/PackageListSerivce.cs
@@ -12,6 +12,7 @@ using System.IO.Packaging;
 using WSIMS_ERP.Shared;
 using WSIMS_ERP.Shared.Models;
 using WSIMS_ERP.Shared.Models.ConfigModel;
+using WSIMS_ERP.Shared.Queries;
 using WSIMS_ERP.Shared.Services;
 
 namespace ERP.Warehouse.Api.Features.Package.PackageList;
@@ -39,113 +40,17 @@ public class PackageListSerivce : AuthorizationService
         PackageRepModel model = new();
         try
         {
-            #region Check User
-
-            var user = await _db.TblWarehouseUsers
-                .AsNoTracking()
-                .FirstOrDefaultAsync(x => x.WarehouseUserId == "01KVNHVBPTSARA9NFQ8NYSQS1F");
-            if (user is null)
+            var parameters = new
             {
-                return Result<PackageRepModel>.Error(JsonResource.WHE001);
-            }
-
-            #endregion
-
-            #region Package
-
-            var userPackages =  await _db.TblPackages
-                .AsNoTracking()
-                .Where(x => x.BranchCode == user.BranchCode)
-                .ToListAsync();
-
-            var existingCodes = userPackages.Select(x => x.PackageInfoCode).ToList();
-
-            var otherPackages = await _db.TblPackages
-                .AsNoTracking()
-                .Where(x => x.BranchCode != user.BranchCode &&
-                            !existingCodes.Contains(x.PackageInfoCode))
-                .ToListAsync();
-
-            var distinctOtherPackages = otherPackages
-                .GroupBy(x => x.PackageInfoCode)
-                .Select(g => g.First());
-
-            #endregion
-
-            #region PackageInfo
-
-            var allPackageInfoCodes = userPackages
-                .Concat(distinctOtherPackages)
-                .Select(x => x.PackageInfoCode)
-                .ToList();
-
-            var packageInfo = await _db.TblPackageInfos
-                .AsNoTracking()
-                .Where(x => allPackageInfoCodes.Contains(x.PackageInfoCode))
-                .ToListAsync();
-
-            var infoDict = packageInfo.ToDictionary(x => x.PackageInfoCode);
-
-            #endregion
-
-            #region Product
-
-            var allProductCodes = packageInfo.Select(x => x.ProductCode).ToList();
-
-            var productCode = await _db.TblProducts
-                .AsNoTracking()
-                .Where(x => allProductCodes.Contains(x.ProductCode))
-                .ToListAsync();
-
-            var product = productCode.ToDictionary(x => x.ProductCode);
-
-            #endregion
-
-            #region Box
-
-            var allBox = packageInfo.Select(x => x.BoxCode).ToList();
-
-            var boxCode = await _db.TblBoxes
-                .AsNoTracking()
-                .Where(x => allBox.Contains(x.BoxCode))
-                .ToListAsync();
-
-            var box = boxCode.ToDictionary(x => x.BoxCode);
-
-            #endregion
-
-            #region Prepare Data
-
-            var combinedPackages = userPackages
-                .Concat(distinctOtherPackages)
-                .Select(x =>
-                {
-                    infoDict.TryGetValue(x.PackageInfoCode, out var info);
-                    product.TryGetValue(info.ProductCode, out var prod);
-                    box.TryGetValue(info.BoxCode, out var boxSize);
-                    {
-                        return new PackageModel
-                        {
-                            PackageId = x.PackageId,
-                            PackageName = info.PackageName,
-                            ProductCode = prod.ProductName,
-                            Price = info.Price.ToString(),
-                            CurrencyCode = info.CurrencyCode,
-                            Weight = info.Weight.ToString(),
-                            BoxCode = boxSize.Size,
-                            PackageInfoCode = x.PackageInfoCode,
-                            Quanity = x.BranchCode != user.BranchCode ? "0" : x.Quanity.ToString(),
-                        };
-                    }
-                    
-                })
-                .ToList();
-
-            model.list = combinedPackages;
-
+                CurrentUserId = AuthorizedUserId,
+                PackageName = reqModel.PackageName,
+                ProductName = reqModel.ProductCode,
+                Box = reqModel.BoxCode,
+            };
+            var result = await _dapperService.QueryStoredProcedureAsync<PackageModel>
+                (SqlQueries.Sp_GetPackageList, parameters);
+            model.list = result;
             return Result<PackageRepModel>.Success(model);
-
-            #endregion
         }
         catch (Exception ex)
         {
--- WSIMS-ERP.Shared/Queries/SqlQueries.cs
+++ WSIMS-ERP.Shared/Queries/SqlQueries.cs
@@ -26,6 +26,8 @@ public class SqlQueries
 
     public const string Sp_GetReqProductChangesDetail = "Sp_GetReqProductChangesDetail";
 
+    public const string Sp_GetPackageList = "Sp_GetPackageList";
+
     public const string Sp_GetReqPackageList = "Sp_GetReqPackageList";
     public const string Sp_GetReqPackageDetail = "Sp_GetReqPackageDetail";
 }
